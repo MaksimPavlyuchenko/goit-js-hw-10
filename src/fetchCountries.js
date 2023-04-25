ee9b8f0ba50e96fa84b421440245788de0127653
MaksimPavlyuchenko <maksimpavlyuchenko82@gmail.com>
2023-04-25 11:25:06 +0300

    hw-10-try-2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 function fetchCountries(country) {
   const BASE_URL = 'https://restcountries.com/v3.1/name/';
-  const URL = `${BASE_URL}${country}`;
+  const URL = `${BASE_URL}${country}?fields=capital,population,flags,languages,name`;
   return fetch(URL).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
